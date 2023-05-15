87b1abd7e749367b46edf8d002acd5526f1e6503
Josh Burnett <joshuaburnett429@gmail.com>
2023-05-15 11:05:55 -0500

    test for spinner added

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,22 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+
+import Spinner from "./Spinner";
+import React from 'react';
+import { render, screen, waitfor } from '@testing-library/react';
+
+test("renders the spinner",() => {
+  render(Spinner);
+
+  expect(Spinner).toBeInTheDocument;
+})
+
+test("renders the spinner when passed on", () => {
+  render(<Spinner on={true}/>);
+  expect(Spinner).toBeInTheDocument;
+})
+
+test("returns null when passed off", () => {
+  render(<Spinner on={false}/>);
+  expect(Spinner).not.toBeInTheDocument;
 })
